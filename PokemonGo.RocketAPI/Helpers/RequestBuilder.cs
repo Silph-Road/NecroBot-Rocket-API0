d53856ba25546b37dcf81a718456c24251991dc2
아 이 딛 <aididhaiqal@users.noreply.github.com>
2016-09-01 11:55:17 +0800

    Update RequestBuilder.cs
    Add support for latest POGOProtos C# (https://github.com/aididhaiqal/POGOProtos)

--- PokemonGo.RocketAPI/Helpers/RequestBuilder.cs
+++ PokemonGo.RocketAPI/Helpers/RequestBuilder.cs
@@ -1,9 +1,12 @@
-﻿using Google.Protobuf;
+using System;
+using System.Collections.Generic;
+using Google.Protobuf;
 using PokemonGo.RocketAPI.Enums;
 using POGOProtos.Networking.Envelopes;
 using POGOProtos.Networking.Requests;
-using System;
-using System.Collections.Generic;
+using POGOProtos.Networking.Platform;
+using static POGOProtos.Networking.Envelopes.RequestEnvelope.Types;
+using static POGOProtos.Networking.Envelopes.Signature.Types;
 
 namespace PokemonGo.RocketAPI.Helpers
 {
@@ -57,37 +60,37 @@ namespace PokemonGo.RocketAPI.Helpers
                 crypt = new Crypt();
         }
 
-        private Unknown6 GenerateSignature(IEnumerable<IMessage> requests)
+        private PlatformRequest GenerateSignature(IEnumerable<IMessage> requests)
         {
             var ticketBytes = _authTicket.ToByteArray();
-
             var sig = new Signature()
             {
-				Timestamp = (ulong)Utils.GetTime(true),
+                Timestamp = (ulong)Utils.GetTime(true),
                 TimestampSinceStart = (ulong)(Utils.GetTime(true) - _startTime),
                 LocationHash1 = Utils.GenerateLocation1(ticketBytes, _latitude, _longitude, _altitude),
                 LocationHash2 = Utils.GenerateLocation2(_latitude, _longitude, _altitude),
-                SensorInfo = new Signature.Types.SensorInfo()
+                SensorInfo = new SensorInfo()
                 {
-                    AccelNormalizedX = GenRandom(-0.31110161542892456, 0.1681540310382843),
-                    AccelNormalizedY = GenRandom(-0.6574847102165222, -0.07290205359458923),
-                    AccelNormalizedZ = GenRandom(-0.9943905472755432, -0.7463029026985168),
+
                     TimestampSnapshot = (ulong)(Utils.GetTime(true) - _startTime - RandomDevice.Next(100, 400)),
-                    MagnetometerX = GenRandom(-0.139084026217, 0.138112977147),
-                    MagnetometerY = GenRandom(-0.2, 0.19),
-                    MagnetometerZ = GenRandom(-0.2, 0.4),
-                    AngleNormalizedX = GenRandom(-47.149471283, 61.8397789001),
-                    AngleNormalizedY = GenRandom(-47.149471283, 61.8397789001),
-                    AngleNormalizedZ = GenRandom(-47.149471283, 5),
-                    AccelRawX = GenRandom(0.0729667818829, 0.0729667818829),
-                    AccelRawY = GenRandom(-2.788630499244109, 3.0586791383810468),
-                    AccelRawZ = GenRandom(-0.34825887123552773, 0.19347580173737935),
+                    LinearAccelerationX = GenRandom(-0.31110161542892456, 0.1681540310382843),
+                    LinearAccelerationY = GenRandom(-0.6574847102165222, -0.07290205359458923),
+                    LinearAccelerationZ = GenRandom(-0.9943905472755432, -0.7463029026985168),
+                    MagneticFieldX = GenRandom(-0.139084026217, 0.138112977147),
+                    MagneticFieldY = GenRandom(-0.2, 0.19),
+                    MagneticFieldZ = GenRandom(-0.2, 0.4),
+                    RotationVectorX = GenRandom(-47.149471283, 61.8397789001),
+                    RotationVectorY = GenRandom(-47.149471283, 61.8397789001),
+                    RotationVectorZ = GenRandom(-47.149471283, 5),
                     GyroscopeRawX = GenRandom(-0.9703824520111084, 0.8556089401245117),
                     GyroscopeRawY = GenRandom(-1.7470258474349976, 1.4218578338623047),
                     GyroscopeRawZ = GenRandom(-0.9681901931762695, 0.8396636843681335),
+                    GravityX = GenRandom(0.0729667818829, 0.0729667818829),
+                    GravityY = GenRandom(-2.788630499244109, 3.0586791383810468),
+                    GravityZ = GenRandom(-0.34825887123552773, 0.19347580173737935),
                     AccelerometerAxes = 3
                 },
-                DeviceInfo = new Signature.Types.DeviceInfo()
+                DeviceInfo = new DeviceInfo()
                 {
                     DeviceId = settings.DeviceId,
                     AndroidBoardName = settings.AndroidBoardName,
@@ -105,7 +108,7 @@ namespace PokemonGo.RocketAPI.Helpers
                 }
             };
 
-            sig.LocationFix.Add(new Signature.Types.LocationFix()
+            sig.LocationFix.Add(new LocationFix()
             {
                 Provider = "fused",
                 Latitude = (float)_latitude,
@@ -125,18 +128,15 @@ namespace PokemonGo.RocketAPI.Helpers
             //sig.Unknown25 = -8537042734809897855; // For 0.33
             sig.Unknown25 = 7363665268261373700; // For 0.35
 
-            Unknown6 val = new Unknown6()
+            var SendEncryptedSignatureRequest = new PlatformRequest()
             {
-                RequestType = 6,
-                Unknown2 = new Unknown6.Types.Unknown2()
-                {
-                    EncryptedSignature = ByteString.CopyFrom(crypt.Encrypt(sig.ToByteArray()))
-                }
+                Type = PlatformRequestType.SendEncryptedSignature,
+                RequestMessage = ByteString.CopyFrom(crypt.Encrypt(sig.ToByteArray()))
             };
 
-            return val;
+            return SendEncryptedSignatureRequest;
         }
-        
+
         public RequestEnvelope GetRequestEnvelope(params Request[] customRequests)
         {
             var e = new RequestEnvelope
@@ -149,11 +149,11 @@ namespace PokemonGo.RocketAPI.Helpers
                 //Unknown6 = , //6
                 Latitude = _latitude, //7
                 Longitude = _longitude, //8
-                Altitude = _altitude, //9
+                Accuracy = _altitude, //9 //Temp
                 AuthTicket = _authTicket, //11
                 MsSinceLastLocationfix = RandomDevice.Next(800, 1900) //12
             };
-            e.Unknown6.Add(GenerateSignature(customRequests));
+            e.PlatformRequests.Add(GenerateSignature(customRequests));
             return e;
         }
 
@@ -169,11 +169,11 @@ namespace PokemonGo.RocketAPI.Helpers
                 //Unknown6 = , //6
                 Latitude = _latitude, //7
                 Longitude = _longitude, //8
-                Altitude = _altitude, //9
-                AuthInfo = new RequestEnvelope.Types.AuthInfo
+                Accuracy = _altitude, //9 //Temp
+                AuthInfo = new AuthInfo
                 {
                     Provider = _authType == AuthType.Google ? "google" : "ptc",
-                    Token = new RequestEnvelope.Types.AuthInfo.Types.JWT
+                    Token = new AuthInfo.Types.JWT
                     {
                         Contents = _authToken,
                         Unknown2 = 59
